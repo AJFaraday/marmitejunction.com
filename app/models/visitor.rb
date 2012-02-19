ead7052fcb62e058e8d4cc904b4628ea5544383d
A Faraday <andrewfaraday@hotmail.co.uk>
2012-02-19 00:23:59 +0000

    changed visitor book validation approach

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -3,6 +3,7 @@ class Visitor < ActiveRecord::Base
   validates_presence_of :name, :email, :message
   validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ , :message => " doesn't look like an email address."
   validates_length_of :message, :maximum => 300
+  validate :no_html_allowed
 
   after_create :set_reported_false
 
@@ -10,9 +11,17 @@ class Visitor < ActiveRecord::Base
 
   def set_reported_false
     if message.include? '</a>'
-      self.destroy
+      #self.destroy
+      update_attribute :reported, true
     end
     update_attribute :reported, false
+  end
+
+  def no_html_allowed
+    puts message
+    if message.include? '>' and message.include? '</'
+      errors.add(:message, 'cannot include html tags.')
+    end
   end
 
 end
